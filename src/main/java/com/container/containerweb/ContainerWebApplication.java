41b2b06b0691b039f8acb84208462b1a314ce272
linfeng0219 <460640866@qq.com>
2018-09-01 14:08:20 +0800

    Initial commit

---
+++ src/main/java/com/container/containerweb/ContainerWebApplication.java
@@ -0,0 +1,12 @@
+package com.container.containerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContainerWebApplication.class, args);
+    }
+}
